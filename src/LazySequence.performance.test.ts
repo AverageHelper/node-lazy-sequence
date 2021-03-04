a5623e5adbc0a3826f0e672bf7f7e52a756f274e
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2021-03-04 14:11:16 -0700

    Try 100k for CI

--- src/LazySequence.performance.test.ts
+++ src/LazySequence.performance.test.ts
@@ -3,7 +3,7 @@ import type LazySequence from "./LazySequence";
 
 describe("LazySequence performance", () => {
   test("lazy map is no slower than normal map", () => {
-    const arraySize = 80000;
+    const arraySize = 100000;
     const mapping = (n: number) => n * n;
 
     const prepareNormalArray = () => randomArray(arraySize);
@@ -18,7 +18,7 @@ describe("LazySequence performance", () => {
   });
 
   test("lazy map and filter is no slower than normal map and filter", () => {
-    const arraySize = 80000;
+    const arraySize = 100000;
     const mapping = (n: number) => n * n;
     const evens = (n: number) => n % 2 === 0;
     const toString = (n: number) => n.toString();
